e15a1717fbb98934e3696ee88acf697645cc1491
Yunus emre salcan <yunusemresalcan@gmail.com>
2024-01-01 15:36:17 +0300

    Caption editor and previewer for AWS Transcribe output

--- voicelyric/src/app/[filename]/page.js
+++ voicelyric/src/app/[filename]/page.js
@@ -1,11 +1,15 @@
 "use client"
 
+import BoltIcon from "@/components/BoltIcon";
+import TranscriptionItem from "@/components/TranscriptionItem";
+import { clearTranscriptionItems } from "@/libs/awsTranscriptionHelpers";
 import axios from "axios"
 import { useEffect, useState } from "react";
 
 export default function newFilepage({ params }) {
     const filename = params.filename
     const [isTranscribing, setIsTranscribing] = useState(false)
+    const [isFetchingInfo, setisFetchingInfo] = useState(false)
     const [awsTranscriptionItems, setAwsTranscriptionItems] = useState([])
 
     useEffect(() => {
@@ -14,7 +18,9 @@ export default function newFilepage({ params }) {
 
 
 function getTranscription(){
+    setisFetchingInfo(true);
     axios.get('/api/transcribe?filename=' + filename).then((res) => {
+        setisFetchingInfo(false);
         const status = res.data.status;
         const transcription = res.data?.transcription;
         if (status === 'IN_PROGRESS') {
@@ -23,34 +29,66 @@ function getTranscription(){
         }
         else{
              setIsTranscribing(false);
-                setAwsTranscriptionItems(transcription.results.items);
+           
+                setAwsTranscriptionItems(
+                    clearTranscriptionItems(transcription.results.items));
         }
-
-
-
-
      });
+}
+
+if (isTranscribing) {
+    return (
+        <div>
+            Videonuz çevriliyor...
+        </div>
+    )
+}
+
+if (isFetchingInfo) {
+    return (
+        <div>
+            Bilgileriniz alınıyor...
+        </div>
+    )
 }
 
 
     return (
         <div>
-            {filename}
-            <div>is transcribing: {JSON.stringify(isTranscribing)}</div>
-            {awsTranscriptionItems.length > 0 && 
-            awsTranscriptionItems.map((item) => (
-                <div>
-                    <span className="text-black/50">
-                        {item.start_time} - {item.end_time}
-                    </span>
-                    <span>
-                        {item.alternatives[0].content}
-                    </span>
-                </div>
-
+            <div className="grid grid-cols-2 gap-16">
+ 
+            <div className="max-w-xs">
+                <h2 className="text-2xl mb-4 text-slate-500">Çeviri</h2>
+            <div className="grid grid-cols-3 sticky top-0 bg-slate-500/50 p-2 
+            rounded-md">
+                <div>Başlangıç  </div>
+                <div>Bitiş  </div>
+                <div>İçerik </div>
+            </div>
+            {awsTranscriptionItems.length > 0 && awsTranscriptionItems.map((item) => (<TranscriptionItem item = {item} />
             ))}
+            </div>
+            <div>
+            <h2 className="text-2xl mb-4 text-slate-500">Video</h2>
+            <div className="mb-4">
+                <button
+                className="bg-green-400 py-2 px-6 
+                rounded-full inline-flex gap-2 border-2 border-green-700
+                cursor-pointer">
+                    <BoltIcon/>
+                    <span> Alt yazıyı ekle</span>
+                </button>
+            </div>
+            <div className="rounded-xl overflow-hidden">
+            <video 
+            controls
+            src={"https://voicelyric.s3.eu-north-1.amazonaws.com/" + filename}>
 
-            
+            </video>
+            </div>
+         
+            </div>
         </div>
+    </div>
     )
 }   
